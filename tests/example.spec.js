51ec5b1653147a7508c9b379da95db1e44400446
Sarthak Gupta <sgsarthak@gmail.com>
2024-10-02 09:21:45 +0200

    inital commit

---
+++ tests/example.spec.js
@@ -0,0 +1,19 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.describe('Funda website', ()=> {
+
+
+  // Add page objects, Add cookie handling
+  // Add Secret handling via github
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
+})
